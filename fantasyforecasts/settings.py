7b55ba09a9402281e8edb1ceba94b4703185ea32
Sarah German <sarah@advomatic.com>
2016-10-08 15:29:59 -0500

    Add database models for forecasts and migrate

--- fantasyforecasts/settings.py
+++ fantasyforecasts/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'forecasts.apps.ForecastsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- forecasts/models.py
+++ forecasts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Forecast(models.Model):
+    date = models.DateTimeField('forecast date')
+    high_temp = models.IntegerField(default=0)
+    low_temp = models.IntegerField(default=0)
